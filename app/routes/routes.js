14286c36700e01b11d8bf2e0b8aa3b19cb35ab6a
CellarD0-0r <alan.turing39@gmail.com>
2018-04-15 23:25:23 +0300

    movies: full text query and extended search

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,30 @@
 module.exports = (app, db) => {
+    // var ObjectId = require('mongodb').ObjectId;
+
     app.get('/movies', (req, res) => {
-        res.send('GET /movies');
+        let _params = {};
+        for (let property in req.query) {
+            if (req.query.hasOwnProperty(property) && property !== 'title') {
+              if (property === 'director') {
+                _params[property] = {$regex: req.query[property]};
+              } else {
+                _params[property] = req.query[property];
+              }
+            }
+        }
+        if (req.query.title !== undefined) {
+            _params.title = {$regex: req.query.title};
+        }
+        console.log('GET /movies', _params);
+        db.collection('movies').find(_params).toArray((err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+
+        // db.collection('movies').find(ObjectId('5ad391d2d8708f354ccf4f1b')).toArray((err, result) => {
+        //     if (err) throw err;
+        //     console.log(result);
+        // });
     });
 
     app.get('/movies/id', (req, res) => {
@@ -12,11 +36,22 @@ module.exports = (app, db) => {
     });
 
     app.post('/movies', (req, res) => {
-        res.send('POST /movies');
-    });
-
-    app.get('/genres', (req, res) => {
-        res.send('GET /genres');
+        let _movie = {
+            title: req.body.title,
+            director: req.body.director,
+            year: req.body.year,
+            genre: req.body.genre,
+            rating: req.body.rating
+        }
+        db.collection('movies').insertOne(_movie, (error, result) => {
+            if (error) {
+                res.send({ 'error': 'An error has occured'} );
+                // res.status(500).json(error);
+            } else {
+                res.send(result.ops);
+            }
+        });
+        // res.send('POST /movies');
     });
 
     app.get('/users', (req, res) => {
--- server.js
+++ server.js
@@ -6,15 +6,86 @@ const cors = require('cors');
 const app = express();
 
 const port = 8000;
+const url = 'mongodb://localhost:27017/db';
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
 
-app.get('/', (req, res) => {
-    res.send('INDEX');
+// mongoClient.connect(url, (err, db) => {
+//     if (err) throw err;
+//     console.log('Database connected');
+//     require('./app/routes')(app, db);
+//     app.listen(port, () => {
+//         console.log('listening port ' + port);
+//     });
+
+//     db.close();
+// });
+
+mongoClient.connect(url, (err, client) => {
+    if (err) throw err;
+    console.log('Database connected');
+    var db = client.db('db');
+    require('./app/routes')(app, db);
+    app.listen(port, () => {
+        console.log('listening port ' + port);
+    });
+    initDb(db, () => {
+        console.log('init db callback');
+    });
+    // client.close();
 });
 
-app.listen(port, () => {
-    console.log('listening port ' + port);
-});
+var initDb = (db, callback) => {
+    // initMovies(db, callback);
+    // clearDb(db, callback);
+};
+
+var clearDb = (db, callback) => {
+    db.collection('movies').removeMany({});
+};
+
+var initMovies = (db, callback) => {
+    db.createCollection('movies', { capped: false, autoIndexId: true}, (err, result) => {
+        console.log('Collection "movies" created');
+        callback();
+    });
+    db.collection('movies').insertMany([
+        {
+            title: 'Аризонская мечта',
+            director: 'Эмир Кустурица',
+            year: '1993',
+            genre: [ 'драма', 'мелодрама', 'комедия' ],
+            rating: '10'
+        },
+        {
+            title: 'Донни Дарко',
+            director: 'Ричард Келли',
+            year: '2001',
+            genre: [ 'фантастика', 'триллер', 'драма' ],
+            rating: '10'
+        },
+        {
+            title: 'Семь психопатов',
+            director: 'Мартин МакДона',
+            year: '2012',
+            genre: [ 'комедия', 'криминал' ],
+            rating: '10'
+        },
+        {
+            title: 'Трасса 60',
+            director: 'Боб Гейл',
+            year: '2001',
+            genre: [ 'фантастика', 'драма', 'комедия' ],
+            rating: '9'
+        },
+        {
+            title: 'Отель "Гранд Будапешт"',
+            director: 'Уэс Андерсон',
+            year: '2014',
+            genre: [ 'комедия', 'детектив', 'приключения' ],
+            rating: '9'
+        }], (err, result) => {
+            console.log('Movies data inserted');
+        });
+};
